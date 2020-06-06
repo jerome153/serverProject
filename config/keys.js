57251b99e6b661af4824fd20a046643c4ae043c6
Jerome <jeromebar234@hotmail.com>
2020-06-06 06:06:51 -0700

    make it better

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+  googleClientID:'1023000135403-6qv7jurl9ov0dn3tfv8f48l95nsan2p8.apps.googleusercontent.com',
+  googleClientSecret:'jDl6vErmrV828lDuXEyKbqh3',
+
+  mongoURI: 'mongodb+srv://p00gz:fewfef323@emaily-h6r2b.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'rvq343dafstv',
+  //eWpAIpyt9HEtadOg
+  //mongodb+srv://myUser:fewfef323@cluster0.e9dfe.mongodb.net/test?retryWrites=true&w=majority
+  //mongodb+srv://myUser:<password>@cluster0-e9dfe.mongodb.net/test?retryWrites=true&w=majority
+  //mongodb+srv://p00gz:fewfef323@emaily-h6r2b.mongodb.net/test?retryWrites=true&w=majority
+  //
+
+
+};
