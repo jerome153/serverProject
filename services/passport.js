69989183e146a4e0c85fdbee36568341190648af
jerome153 <56744637+jerome153@users.noreply.github.com>
2020-06-17 14:37:42 -0700

    what ever man

--- services/passport.js
+++ services/passport.js
@@ -28,14 +28,27 @@ passport.use(
 
     },
     async (accessToken, refreshToken, profile, done) => {
-    const existingUser = await User.findOne({ googleId: profile.id })
-        if (existingUser) {
-
-          done(null, existingUser);
+      const existingUser = await User.findOne({ googleId: profile.id })
+        if(existingUser) {
+          return done(null, existingUser);
         }
-          const user = await new User({ googleId: profile.id }).save()
-          done(null, user);
-        }
+        const user = await new User({ googleId: profile.id}).save();
+        done(null, user);
+      })
+  );
 
-  )
-);
+
+
+    //(accessToken, refreshToken, profile, done) => {
+    //  User.findOne({googleId: profile.id }).then(existingUser => {
+      //  if(existingUser){
+      //    done(null, existingUser);
+      //  }else {
+        //  new User({ googleId: profile.id})
+        //  .save()
+      //    .then(user => done(null, user));
+      //  }
+    //  });
+  //  }
+//  )
+//);
